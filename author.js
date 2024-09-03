79815cdfcfbcba444da8e14a58c49e993cf8f204
Gábor Csillag <gcsillag.dev@gmail.com>
2024-09-03 18:23:38 +0200

    some refact

--- author.js
+++ author.js
@@ -16,7 +16,7 @@ function appendAuthorToHTML() {
 
         const emailLabel = document.createTextNode("Email: ")
         const authorMailAnchor = document.createElement("a");
-        authorMailAnchor.className = "link-footer";
+        authorMailAnchor.className = "link";
         authorMailAnchor.href = `mailto:${authorMail}`;
         authorMailAnchor.textContent = authorMail;
 
